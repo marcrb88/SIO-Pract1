b390cfe8faa87ff8df746f3e916d1f803ffd25d0
Pau Fernandez Bardulet <paufernandezbardulet@gmail.com>
2023-04-21 22:02:12 +0200

    Average price by property type (Edinburgh, Santiago)

---
+++ avg-price-by-property-type-santiago.R
@@ -0,0 +1,16 @@
+library(RMySQL)
+library(ggplot2)
+
+lapply(dbListConnections(dbDriver(drv = "MySQL")), dbDisconnect)
+conn <- dbConnect(MySQL(), user="root", password="", dbname="pract1", host="localhost")
+
+result <- dbSendQuery(conn, "SELECT price, property_type FROM santiago")
+data <- dbFetch(result, n=-1)
+df <- data.frame(data)
+
+ggplot(df, aes(x = property_type, y = price)) +
+  stat_summary(fun = mean, geom = "bar", fill = "blue") +
+  labs(x = "Property Type", y = "Price", title = "Average Price by Property Type") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggsave("avg_price_by_property_type_santiago.png", width = 11, height = 4, dpi = 300)
--- graphs.R
+++
@@ -1,9 +0,0 @@
-library(RMySQL)
-lapply(dbListConnections(dbDriver(drv = "MySQL")), dbDisconnect)
-conn <- dbConnect(MySQL(), user="root", password="", dbname="pract1", host="localhost")
-result <- dbSendQuery(conn, "SELECT price FROM edinburgh")
-data <- dbFetch(result, n=-1)
-prices <- as.numeric(data$price)
-freq_table <- table(prices)
-freq_table_pct <- prop.table(freq_table) * 100  # calculate relative frequency and convert to percentage
-barplot(freq_table_pct, main="Frequency Distribution of Prices in Edinburgh", xlab="Price", ylab="Percentage", ylim=c(0,2.5))
