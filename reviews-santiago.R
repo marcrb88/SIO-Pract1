ab0b7c7b7807c4efa5a48e60215f7292da87e852
Pau Fernandez Bardulet <paufernandezbardulet@gmail.com>
2023-04-22 14:06:36 +0200

    review_scores Pearson correlation coefficients (Edinburgh, Santiago)

---
+++ reviews-santiago.R
@@ -0,0 +1,10 @@
+library(RMySQL)
+
+lapply(dbListConnections(dbDriver(drv = "MySQL")), dbDisconnect)
+conn <- dbConnect(MySQL(), user="root", password="", dbname="pract1", host="localhost")
+
+result <- dbSendQuery(conn, "SELECT review_scores_cleanliness, review_scores_checkin, review_scores_communication, review_scores_location FROM reviews_santiago")
+data <- dbFetch(result, n=-1)
+df <- data.frame(data)
+
+print(cor(df))
